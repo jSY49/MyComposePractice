2789cf45c69b55b7b740de51ba2a83d5f2552168
jsy49 <jseung49@gmail.com>
2026-03-04 21:42:15 +0900

    box,row,column basic

--- app/src/main/java/com/jaysdevapp/mycomposepractice/MainActivity.kt
+++ app/src/main/java/com/jaysdevapp/mycomposepractice/MainActivity.kt
@@ -2,31 +2,38 @@ package com.jaysdevapp.mycomposepractice
 
 import android.os.Bundle
 import android.util.Log
+import android.widget.Toast
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
 import androidx.compose.material3.Button
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -131,11 +138,55 @@ fun ConstraintLayoutContent() {
     }
 }
 
+@Composable
+fun profileCard(){
+
+    val context = LocalContext.current  //함수 안에서 this를 쓰면, Activity를 가리키지 않음
+
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .padding(10.dp)
+            .background(Color.LightGray),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+
+        Row (
+            modifier = Modifier.background(Color.Yellow),
+            verticalAlignment = Alignment.CenterVertically
+        ){
+            Box(
+                modifier = Modifier
+                    .size(50.dp)
+                    .background(Color.DarkGray)
+            ) {
+
+            }
+            Spacer(modifier = Modifier.height(20.dp).width(10.dp))
+            Column(
+
+            ) {
+                Text(text = "ID: jsy", style = MaterialTheme.typography.headlineMedium)
+                Text(text = "jetpack compose 다시 공부 중", style = MaterialTheme.typography.bodySmall)
+            }
+
+        }
+        Spacer(modifier = Modifier.height(10.dp))
+        Button(onClick = {
+            Toast.makeText(context,"click! ", Toast.LENGTH_SHORT).show()
+        }) {
+            Text("click")
+        }
+    }
+}
+
 @Preview(showBackground = true)
 @Composable
 fun GreetingPreview() {
     MyComposePracticeTheme {
 //        MyFirstScreen()
-        ConstraintLayoutContent()
+//        ConstraintLayoutContent()
+        profileCard()
     }
 }
